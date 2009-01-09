900966655497930a9e0ade93f6fc9874bafe1631
towski <towski@gmail.com>
2009-01-08 18:55:02 -0800

    handling redirects

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -7,6 +7,7 @@ class StatusController < ApplicationController
 		@user.save if @user.new_record?
     Delayed::Job.enqueue Job::FetchStatuses.new(@user.id)
 		@statuses = @user.statuses + Status.all(:conditions => {:user_id => @facebook_user.friends.map(&:id)})
+		@status = Status.new(params[:status])
 	end
 	
 	def create
@@ -17,7 +18,10 @@ class StatusController < ApplicationController
   	  @user.statuses.create! :message => params[:status][:message]
   	  redirect_to '/'
     rescue Facebooker::Session::ExtendedPermissionRequired => e
-      redirect_to "http://www.facebook.com/authorize.php?api_key=#{ENV['FACEBOOK_API_KEY']}&ext_perm=status_update&v=1.0"
+      redirect_to url_for(:host => "www.facebook.com", 
+        :action => "authorize.php", :api_key => ENV['FACEBOOK_API_KEY'], 
+        :v => "1.0", :ext_perm => "status_update", :next => root_url(:status => params[:status]), 
+        :next_cancel => root_url(:status => params[:status]))
     end
   end
 end
